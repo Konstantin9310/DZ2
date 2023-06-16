a20642ac459a592cc7a3f77ca9affe3327ccca83
Konstantin <alkapone10031993@gmail.com>
2023-06-16 16:43:49 +0300

    first

---
+++ FirstWork/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и
+ на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+*/
+
+Console.Write("Input number => ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+
+Console.WriteLine("second digit=> "+stringNumber[1]);
